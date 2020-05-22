08b021e0dac43a3595f54186f7168637af7f2526
Kimberly Boyd <kimberlyboyd@Kimberlys-MacBook-Pro.local>
2020-05-21 20:14:26 -0400

    login form done, not styled

--- secret-family-recipes/src/components/LoginForm.js
+++ secret-family-recipes/src/components/LoginForm.js
@@ -2,7 +2,13 @@ import React, { useState, useEffect } from "react";
 import * as yup from "yup";
 import axios from "axios";
 
-//  username & password
+
+
+// validation schema
+const formSchema = yup.object().shape({
+    username: yup.string().required("Please enter your username"),
+    password: yup.string().required("Please enter your password")
+});
 
 
 export default function LoginForm() {
@@ -12,14 +18,65 @@ export default function LoginForm() {
         password: ""
     };
 
+
+    // States
     const [formState, setFormState] = useState(initialState);
     const [errorState, setErrorState] = useState(initialState);
 
-    // not allowed to submit unless form is complete
+
+    // only allow form submit upon completion of all fields
     const [buttonDisabled, setButtonDisabled] = useState(true);
+    
+    useEffect(() => {
+     formSchema.isValid(formState).then(valid => { setButtonDisabled(!valid);});
+        }, [formState]);
+
+
+     // validation
+    const validate = e => {
+            yup
+                .reach(formSchema, e.target.name)
+                .validate(e.target.value)
+                .then(valid => {
+                    setErrorState({...errorState, [e.target.name]: ""
+                    });
+                })
+                .catch(err => {
+                    console.log(err.errors);
+                    setErrorState({
+                        ...errorState,
+                        [e.target.name]: err.errors[0]
+                    });
+                });
+    
+        }
+
+    // Change Handler
+    const changeHandler = e => {
+        e.persist();
+        validate(e);
+        setFormState({...formState, [e.target.name]: e.target.value })
+    }
+
+    // Submit Handler
+    const formSubmit = (e) => {
+        e.preventDefault();
+        console.log("Form Submitted!");
+
+        // axios call to dummy API so I could see if my validation worked
+        axios
+             .post("https://reqres.in/api/users", formState)
+            .then((response) => {
+                setFormState(initialState);
+                console.log(response);
+
+        })
+        .catch((err) => console.log(err.response));
+    }
+
 
     return (
-        <form>
+        <form onSubmit={formSubmit}>
           <h3>Login</h3>
             <label htmlFor="username">
                 Username:
@@ -28,8 +85,9 @@ export default function LoginForm() {
                     name="username"
                     id="username"
                     value={formState.username}
-                    // add on change
+                    onChange={changeHandler}
                 />
+                {errorState.username.length > 0 ? (<p className="error">{errorState.username}</p>) : null}
             </label>
             <label htmlFor="password">
                 Password:
@@ -38,9 +96,11 @@ export default function LoginForm() {
                     name="password"
                     id="password"
                     value={formState.password}
-                    // onchange
+                    onChange={changeHandler}
                 />
+                {errorState.password.length > 0 ? (<p className="error">{errorState.password}</p>) : null}
             </label>
+            
             <button disabled={buttonDisabled}>Submit</button>
         </form>
     )
