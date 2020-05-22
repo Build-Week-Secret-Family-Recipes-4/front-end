f7ab5f704f8a2e46292c4c4bec00d94fa0c30a0c
Taylor Palmer <taylorjadepalmer@gmail.com>
2020-05-22 12:32:35 -0700

    added PrivateRoute, Routes in App.js, Login page

--- secret-family-recipes/src/App.js
+++ secret-family-recipes/src/App.js
@@ -2,8 +2,11 @@ import React, { useState, useEffect } from "react";
 import { Route } from "react-router-dom";
 import "./App.css";
 
+import PrivateRoute from "./components/PrivateRoute";
 import { RecipeContext } from "./contexts/RecipeContext";
 import axiosWithAuth from "./components/axiosWithAuth";
+import RecipeList from "./components/RecipeList";
+import Login from "./components/Login";
 
 function App() {
   const [recipes, setRecipes] = useState([]);
@@ -21,9 +24,10 @@ function App() {
 
   return (
     <div className="App">
-      <RecipeContext.Provider
-        value={{ recipes, setRecipes }}
-      ></RecipeContext.Provider>
+      <RecipeContext.Provider value={{ recipes, setRecipes }}>
+        <Route exact path="/" component={Login} />
+        <PrivateRoute path="recipelist" component={RecipeList} />
+      </RecipeContext.Provider>
     </div>
   );
 }
---
+++ secret-family-recipes/src/components/Login.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const Login = (props) => {
+  // make a post request to retrieve a token from the api
+  // when you have handled the token, navigate to the BubblePage route
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  console.log("login: ", username, password);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    setIsLoading(true);
+    axios
+      .post("http://localhost:5000/api/login", { username, password })
+      .then((res) => {
+        console.log(res);
+        localStorage.setItem("token", res.data.payload);
+        props.history.push("/recipelist");
+        setUsername("");
+        setPassword("");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    setIsLoading(false);
+  };
+
+  return (
+    <div className="login">
+      <h1>Welcome to Secret Family Recipes!</h1>
+      <h2>{isLoading ? "...loading" : "Login"}</h2>
+      <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          name="username"
+          placeholder="username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+};
+
+export default Login;
