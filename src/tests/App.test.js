38e218b2f62222dbc3bb5faf15cda8e34deb80cd
Taylor Palmer <taylorjadepalmer@gmail.com>
2020-05-29 17:41:01 -0700

    deleted unused imports

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, wait, fireEvent } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 
 import App from "../App";
 import RecipeList from "../components/RecipeList";
