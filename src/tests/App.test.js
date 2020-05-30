61bb19769bed9b2e0fe4f757eebc97a0f3eabd8e
Taylor Palmer <taylorjadepalmer@gmail.com>
2020-05-29 17:04:30 -0700

    added some tests

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,7 +3,27 @@ import { render, wait, fireEvent } from "@testing-library/react";
 
 import App from "../App";
 import RecipeList from "../components/RecipeList";
+import LoginForm from "../components/LoginForm";
 
 test("App renders without crashing", () => {
   render(<App />);
+});
+
+test("RecipeList renders without crashing", () => {
+  const { getByText } = render(<RecipeList />);
+
+  getByText(/ingredients/i);
+});
+
+test("LoginForm renders", () => {
+  const { getByText } = render(<LoginForm />);
+
+  getByText(/username/i);
+});
+
+test("LoginForm renders", () => {
+  const { getByText } = render(<LoginForm />);
+
+  const input = getByText(/login/i);
+  fireEvent.mouseDown(input);
 });
